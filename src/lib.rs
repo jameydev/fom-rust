7793e000a34b42474839d1bad78a4272d8b2d3f3
Jamey Bryce <jybryce4@gmail.com>
2023-01-05 21:04:27 -0600

    got player get_inventory test going

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub mod game {
             Elf,
             Ork,
             Halfling,
-            Tiefling,
+            Infernali,
             Dragonkin
         }
     
@@ -23,16 +23,20 @@ pub mod game {
             race: Race,
             health: f32,
             mana: f32,
-            inventory: Vec<String>
+            inventory: Vec<(String, usize)>
         }
     
         impl Player {
-            pub fn new(&self, name: &str, class: Class, race: Race) -> Self {
+            pub fn new(name: &str, class: Class, race: Race) -> Self {
                 Self { name: name.to_string(), class, race, health: 100.0, mana: 100.0, inventory: Vec::new() }
             }
 
-            pub fn add_item(&mut self, item: &str) {
-                self.inventory.push(item.to_string());
+            pub fn add_item(&mut self, item: (String, usize)) {
+                self.inventory.push(item);
+            }
+
+            pub fn get_inventory(&self) -> &Vec<(String, usize)> {
+                &self.inventory
             }
 
             pub fn change_name(&mut self, name: &str) {
@@ -49,8 +53,20 @@ pub mod game {
 
 #[cfg(test)]
 mod game_tests {
+    use std::vec;
+
+    use super::game::player::*;
+
+    fn player_setup() -> Player {
+        Player::new("Gaius", Class::Rogue, Race::Human)
+    }
+
     #[test]
     fn inventory_add_works() {
+        let mut test_player = player_setup();
+        test_player.add_item((String::from("Iron Sword"), 1));
+        test_player.add_item((String::from("Health Potion"), 4));
 
+        assert_eq!(vec![(String::from("Iron Sword"), 1), (String::from("Health Potion"), 4)], *test_player.get_inventory());
     }
 }
